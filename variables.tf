54affab9d9fa2b706a4a79ca42402265512dfa56
Devin Lauderdale <devin.lauderdale@synapsestudios.com>
2023-04-07 16:22:00 -0500

    add standard variables

--- variables.tf
+++ variables.tf
@@ -1,4 +1,26 @@
-variable "example" {
+variable "tags" {
+  type = map(string)
+  default = {
+    "ProvisionedBy" = "terraform"
+    "Environment"   = "dev"
+    "Module"        = "terraform-aws-cost-explorer-anomaly-monitor"
+  }
+  description = "Tags to apply to all resources created, see internal documentation for tagging standards"
+}
+
+variable "sns_topic_arn" {
   type        = string
-  description = "Example variable"
+  description = "The ARN of the SNS topic to send notifications to"
+}
+
+variable "threshold" {
+  type        = string
+  default     = "10"
+  description = "Value between 0 and 100 representing the percentage of impact to trigger an alert"
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "The environment this module is being deployed to"
 }
